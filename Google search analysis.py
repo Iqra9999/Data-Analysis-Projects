5e0cffafb8fedcc59de9c8447d284620d31a6a97
Iqra Batool <149294792+Iqra9999@users.noreply.github.com>
2024-02-11 21:34:43 +0500

    Update Google search analysis.py

--- Google search analysis.py
+++ Google search analysis.py
@@ -1,14 +1,17 @@
+#Google Trends provides an API that can be used to analyze the daily searches on Google. 
 import pandas as pd
 from pytrends.request import TrendReq
 import matplotlib.pyplot as plt
 trends = TrendReq()
 
+#I'll analyze Google search trends related to "Machine Learning" queries in this context.
 trends.build_payload(kw_list=["Machine Learning"])
 data = trends.interest_by_region()
 data = data.sort_values(by="Machine Learning", ascending=False)
 data = data.head(10)
 print(data)
 
+#Visualizing the above data using bar chart.
 data.reset_index().plot(x="geoName", 
                         y="Machine Learning", 
                         figsize=(15,12), kind="bar")
